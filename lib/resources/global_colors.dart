1cbec7ef8355a23c624eed2be06d97cf18451054
oiesc <45150141+oiesc@users.noreply.github.com>
2022-09-10 18:43:47 -0300

    start index page

---
+++ lib/resources/global_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GlobalColors {
+  static const Color navy = Color(0xFF001f3f);
+  static const Color blue = Color(0xFF0074D9);
+  static const Color aqua = Color(0xFF7FDBFF);
+  static const Color teal = Color(0xFF39CCCC);
+  static const Color purple = Color(0xFFB10DC9);
+  static const Color fuchsia = Color(0xFFF012BE);
+  static const Color maroon = Color(0xFF85144b);
+  static const Color red = Color(0xFFFF4136);
+  static const Color orange = Color(0xFFFF851B);
+  static const Color yellow = Color(0xFFFFDC00);
+  static const Color olive = Color(0xFF3D9970);
+  static const Color green = Color(0xFF2ECC40);
+  static const Color lime = Color(0xFF01FF70);
+  static const Color black = Color(0xFF111111);
+  static const Color gray = Color(0xFFAAAAAA);
+  static const Color silver = Color(0xFFDDDDDD);
+  static const Color white = Color(0xFFFFFFFF);
+}
---
+++ lib/views/home/index.dart
@@ -0,0 +1,137 @@
+import 'package:adminapp/resources/global_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(toolbarHeight: 100, title: const AppBarWidget()),
+        body: Column(
+          children: [
+            Row(
+              children: const [
+                IconWidget(
+                  type: "products",
+                ),
+                IconWidget(type: "requests")
+              ],
+            )
+          ],
+        ));
+  }
+}
+
+class IconWidget extends StatelessWidget {
+  const IconWidget({
+    Key? key,
+    required this.type,
+  }) : super(key: key);
+
+  final String type;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(
+          top: 16,
+          left: type == "products" ? 16 : 8,
+          bottom: 16,
+          right: type == "products" ? 8 : 16,
+        ),
+        height: 50,
+        decoration: BoxDecoration(
+          color: type == "products" ? GlobalColors.orange : GlobalColors.green,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: SizedBox(
+              width: 40,
+              height: 40,
+              child: Icon(
+                type == "products"
+                    ? Icons.lunch_dining_rounded
+                    : Icons.format_list_numbered_rounded,
+                color: GlobalColors.white,
+                size: 30,
+              ),
+            ),
+          ),
+          Expanded(
+              child: Text(
+            type == "products" ? 'Products' : 'Requests',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: GlobalColors.white,
+            ),
+          )),
+          const SizedBox(
+            width: 40,
+            height: 40,
+            child: Icon(
+              Icons.expand_more,
+              color: GlobalColors.white,
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            color: GlobalColors.white,
+            borderRadius: BorderRadius.circular(50),
+            image: const DecorationImage(
+              image: AssetImage("assets/images/logo.png"),
+            ),
+          ),
+        ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Text(
+                'Burger Place',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Text(
+                'ADM APP',
+                style: TextStyle(fontSize: 14),
+              ),
+            )
+          ],
+        ))
+      ],
+    );
+  }
+}
